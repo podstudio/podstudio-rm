95f537465cb880f7f12a5c820b8915bb0f153ec4
Nader Hendawi <nader@backupify.com>
2014-02-10 13:16:56 -0500

    Setup Phase Complete

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,8 +4,10 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
-    main_controller = MainController.alloc.initWithNibName(nil, bundle: nil)
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(main_controller)
+    player_controller = PlayerController.new
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(player_controller)
+
+    @window.styleMode = PXStylingNormal
 
     @window.makeKeyAndVisible
     true
